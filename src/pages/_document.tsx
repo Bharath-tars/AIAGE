5800d7605dd70bf95ed5c0e1c6fd42687b1e5f13
Bharath <bharath.sudarsanam04@gmail.com>
2023-07-02 07:44:23 +0530

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,9 @@ export default function Document() {
       <Head>
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+<a href="/">
         <link rel="icon" href="/images/tars_favicon-removebg-preview.png" />
+</a>
       </Head>
       <body>
         <Main />
