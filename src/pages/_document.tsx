e6287c34d49f8db28cdceb1c273936c3de52595a
Bharath <bharath.sudarsanam04@gmail.com>
2023-07-02 08:01:16 +0530

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-<a href="/">
+<a href="https://tarsai.netlify.app/">
         <link rel="icon" href="/images/tars_favicon-removebg-preview.png" />
 </a>
       </Head>
